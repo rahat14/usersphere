2df30ffa5aa2b56bda4c0261daf3a40162823692
rahat-devolic <rahat.shovo@devolic.com>
2025-05-20 02:23:06 +0600

    feat: Add UserDetailsScreen and configure route

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,7 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:usersphere/features/user/presentation/screens/UserDetailsScreen.dart';
 
+import '../features/user/data/models/UserListResp.dart';
 import '../features/user/presentation/screens/UserListScreen.dart';
 
 class AppRouter {
-  static final router = GoRouter(routes: [GoRoute(path: '/', builder: (_, __) => UserListScreen())]);
+  static final router = GoRouter(
+    routes: [
+      GoRoute(path: '/', builder: (_, __) => UserListScreen()),
+      GoRoute(
+        path: '/user-details',
+        pageBuilder: (context, state) {
+          final user = state.extra as User;
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: UserDetailsScreen(user: user),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              return SlideTransition(
+                position: Tween(begin: Offset(0, 0.2), end: Offset.zero).animate(animation),
+                child: FadeTransition(
+                  opacity: animation,
+                  child: child,
+                ),
+              );
+
+            },
+          );
+        },
+      ),
+    ],
+  );
 }
